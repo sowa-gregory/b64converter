73994862be4e038aa23e5d3ef511d9674fb0fcdc
Grzesiek <sowisz@Grzesieks-MBP.lan>
2020-12-02 20:33:26 +0100

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/convert_tob64/:data', (req, res) => {
+    var data = req.params.data;
+    console.log(data);
+    return res.send(Buffer.from(data).toString("base64"));
+  });
+
+var server = app.listen(80, ()=>{console.log("server started")});
